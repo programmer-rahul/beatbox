99f4eb264708fdea76caecbf543639f5c8e60a78
programmer-rahul <7678478061k@gmail.com>
2024-08-30 18:06:06 +0530

    added icons for tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
 
 const TabLayout = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="(home)" />
-      <Tabs.Screen name="saved" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen
+        name="(home)/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Feather size={20} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved/index"
+        options={{
+          title: "Saved",
+          tabBarIcon: ({ color }) => (
+            <Feather size={20} name="heart" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings/index"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Feather size={20} name="settings" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
