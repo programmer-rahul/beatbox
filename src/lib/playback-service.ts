9765e2687da7e39ff33a635715ed5800ea61eb90
programmer-rahul <7678478061k@gmail.com>
2024-09-25 17:12:32 +0530

    added playback events for music

--- src/lib/playback-service.ts
+++ src/lib/playback-service.ts
@@ -1,3 +1,11 @@
-const playbackService = async () => {};
+import TrackPlayer, { Event } from "react-native-track-player";
+
+const playbackService = async () => {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+  TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+  TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.stop());
+  TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext());
+  TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToPrevious());
+};
 
 export default playbackService;
